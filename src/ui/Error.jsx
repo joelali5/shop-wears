cce65e2c83134db62e43982eec17d2a53ee683e7
Joel Aliyu <joelaliyu1@gmail.com>
2024-02-08 23:24:56 +0000

    Styled the 'NotFound' route page

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -1,5 +1,21 @@
+import { useRouteError } from "react-router-dom";
+
 function Error() {
-  return <h1>Error</h1>;
+  const error = useRouteError();
+
+  return (
+    <div className="min-h-dvh flex flex-col justify-center items-center bg-slate-200">
+      <h1 className="text-2xl md:text-3xl font-bold font-roboto">
+        Oops! 🛑 🛑
+      </h1>
+      <p className="text-xl md:text-2xl font-roboto">
+        Sorry, an unexpected error has occurred!
+      </p>
+      <p className="text-2xl md:text-3xl">
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  );
 }
 
 export default Error;
